c6201f44ed7cdb9a471d666c47e6cae8a2d9ae08
Andric LibreSinn <andric.droid@gmail.com>
2020-09-01 22:51:50 -0400

    don't need this no mo

--- scripts/run-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-FILE=$1
-
-echo "----------------------------------------"
-echo "Running file: ${FILE}"
-echo "----------------------------------------"
-
-tsnd --respawn -r @babel/register src/${FILE}.ts
